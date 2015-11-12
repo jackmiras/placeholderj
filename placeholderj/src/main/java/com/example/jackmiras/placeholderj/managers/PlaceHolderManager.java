5bd9b04404730ec4d104dac9b6c90f3c9d972aa7
Jackson <jackmiras@gmail.com>
2015-11-11 23:46:46 -0300

    Adding documentation to the public methods of PlaceHolderManager.java

--- placeholderj/src/main/java/com/example/jackmiras/placeholderj/managers/PlaceHolderManager.java
+++ placeholderj/src/main/java/com/example/jackmiras/placeholderj/managers/PlaceHolderManager.java
@@ -28,10 +28,24 @@ public class PlaceHolderManager {
     private boolean isErrorViewBeingShown;
     private boolean isEmptyViewBeingShown;
 
+    /**
+     * Called when a new instance of this class is create. The constructor will get all views passed
+     * in viewsId using the Activity passed together whit viewsId.
+     *
+     * @param activity The activity used to find the views that will be passeds in viewsId.
+     * @param viewsId  A set of view that will interact between himself.
+     */
     public PlaceHolderManager(Activity activity, int... viewsId) {
         this(activity.getWindow().getDecorView(), viewsId);
     }
 
+    /**
+     * Called when a new instance of this class is create. The constructor will get all views passed
+     * in viewsId using the Activity passed together whit viewsId.
+     *
+     * @param view The view used to find the views that will be passeds in viewsId.
+     * @param viewsId  A set of view that will interact between himself.
+     */
     public PlaceHolderManager(View view, int... viewsId) {
         for (int aViewsId : viewsId) {
             if (aViewsId == R.id.view_loading) {
@@ -56,30 +70,52 @@ public class PlaceHolderManager {
             view.setVisibility(visibility);
     }
 
+    /**
+     * Makes the loading view visible if the loaing view is added in your layout.
+     */
     public void showLoading() {
         isLoadingViewBeingShown = true;
         changeViewsVisibility();
         setViewVisibility(framelayoutViewLoading, View.VISIBLE);
     }
 
-    public void showEmpty(int messageRes, RetrofitError error, View.OnClickListener callback) {
+    /**
+     * Makes the empty view visible if the empty view is added in your layout.
+     *
+     * @param messageRes      The message that will be show in the empty view.
+     * @param error           The retrofit error get of some of your application requests.
+     * @param onClickListener The action that will be performed by the try again button present in empty view layout.
+     */
+    public void showEmpty(int messageRes, RetrofitError error, View.OnClickListener onClickListener) {
         textViewEmptyMessage.setText(messageRes);
-        showEmpty(error, callback);
+        showEmpty(error, onClickListener);
     }
 
-    public void showEmpty(RetrofitError error, View.OnClickListener callback) {
+    /**
+     * Makes the empty view visible if the empty view is added in your layout.
+     *
+     * @param error           The retrofit error get of some of your application requests.
+     * @param onClickListener The action that will be performed by the try again button present in empty view layout.
+     */
+    public void showEmpty(RetrofitError error, View.OnClickListener onClickListener) {
         isEmptyViewBeingShown = true;
         changeViewsVisibility();
         if (error == null && textViewEmptyTryAgain.getVisibility() == View.VISIBLE) {
             textViewEmptyTryAgain.setVisibility(View.GONE);
-        } else if (callback != null){
+        } else if (onClickListener != null) {
             textViewEmptyTryAgain.setVisibility(View.VISIBLE);
-            textViewEmptyTryAgain.setOnClickListener(callback);
+            textViewEmptyTryAgain.setOnClickListener(onClickListener);
         }
         setViewVisibility(linearlayoutViewEmpty, View.VISIBLE);
     }
 
-    public void showError(RetrofitError error, View.OnClickListener callback) {
+    /**
+     * Makes the error view visible if the error view is added in your layout.
+     *
+     * @param error           The retrofit error get of some of your application requests.
+     * @param onClickListener The action that will be performed by the try again button present in error view layout.
+     */
+    public void showError(RetrofitError error, View.OnClickListener onClickListener) {
         isErrorViewBeingShown = true;
         changeViewsVisibility();
         if (error != null && error.getKind() == RetrofitError.Kind.NETWORK) {
@@ -89,29 +125,38 @@ public class PlaceHolderManager {
             imageViewErrorIcon.setImageResource(R.drawable.icon_error_unknown);
             textViewErrorMessage.setText(R.string.global_unknown_error);
         }
-        textViewErrorTryAgain.setOnClickListener(callback);
+        textViewErrorTryAgain.setOnClickListener(onClickListener);
         setViewVisibility(linearlayoutViewError, View.VISIBLE);
     }
 
+    /**
+     * Makes the loading view invisible if the loaing view is added in your layout.
+     */
     public void hideLoading() {
         isLoadingViewBeingShown = false;
         changeViewsVisibility();
         setViewVisibility(framelayoutViewLoading, View.GONE);
     }
 
+    /**
+     * Makes the empty view invisible if the loaing view is added in your layout.
+     */
     public void hideEmpty() {
         isEmptyViewBeingShown = false;
         changeViewsVisibility();
         setViewVisibility(linearlayoutViewEmpty, View.GONE);
     }
 
+    /**
+     * Makes the empty view invisible if the loaing view is added in your layout.
+     */
     public void hideError() {
         isErrorViewBeingShown = false;
         changeViewsVisibility();
         setViewVisibility(linearlayoutViewError, View.GONE);
     }
 
-    public void changeViewsVisibility() {
+    private void changeViewsVisibility() {
         if (!isLoadingViewBeingShown && framelayoutViewLoading != null && framelayoutViewLoading.getVisibility() == View.VISIBLE)
             setViewVisibility(framelayoutViewLoading, View.GONE);
         if (!isEmptyViewBeingShown && linearlayoutViewEmpty != null && linearlayoutViewEmpty.getVisibility() == View.VISIBLE)
