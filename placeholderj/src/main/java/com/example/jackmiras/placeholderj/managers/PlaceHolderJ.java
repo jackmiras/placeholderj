dcacd2dcd17962f816fb5b10dd254ba9e39fdae4
Jackson <jackmiras@gmail.com>
2015-12-14 00:11:45 -0300

    Renaming PlaceHolderManager to PlaceHolderJ and creating a new PlaceHolderManager that is now resposible for getting customizations inputs to customize view of PlaceHolderJ

---
+++ placeholderj/src/main/java/com/example/jackmiras/placeholderj/managers/PlaceHolderJ.java
@@ -0,0 +1,202 @@
+package com.example.jackmiras.placeholderj.managers;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.jackmiras.placeholderj.R;
+
+import retrofit.RetrofitError;
+
+/**
+ * Created by jackmiras on 03/10/15.
+ */
+public class PlaceHolderJ {
+
+    private PlaceHolderManager placeHolderManager;
+    private CustomizeViews customizeViews;
+    private Context context;
+
+    private View viewContainer = null;
+    public View viewLoading = null;
+    public ViewGroup viewEmpty = null;
+    public ImageView viewEmptyImage = null;
+    public TextView viewEmptyMessage;
+    public TextView viewEmptyTryAgainButton = null;
+    public ViewGroup viewError = null;
+    public ImageView viewErrorImage = null;
+    public TextView viewErrorMessage = null;
+    public TextView viewErrorTryAgainButton = null;
+    private boolean isLoadingViewBeingShown;
+    private boolean isErrorViewBeingShown;
+    private boolean isEmptyViewBeingShown;
+    private boolean viewsAreCustomized;
+
+    public PlaceHolderJ() {
+    }
+
+    public PlaceHolderJ(PlaceHolderManager placeHolderManager) {
+        this.placeHolderManager = placeHolderManager;
+        customizeViews = new CustomizeViews(placeHolderManager);
+    }
+
+    /**
+     * Called when a new instance of this class is create. The constructor will get all views passed
+     * in viewsId using the Activity passed together whit viewsId.
+     *
+     * @param activity The activity used to find the views that will be passeds in viewsId.
+     * @param viewsId  A set of view that will interact between himself.
+     */
+    public void init(Activity activity, int... viewsId) {
+        init(activity.getWindow().getDecorView(), viewsId);
+    }
+
+    /**
+     * Called when a new instance of this class is create. The constructor will get all views passed
+     * in viewsId using the Activity passed together whit viewsId.
+     *
+     * @param view    The view used to find the views that will be passeds in viewsId.
+     * @param viewsId A set of view that will interact between himself.
+     */
+    public void init(View view, int... viewsId) {
+        context = view.getContext();
+        for (int aViewsId : viewsId) {
+            if (aViewsId == R.id.view_loading) {
+                viewLoading = view.findViewById(R.id.view_loading);
+            } else if (aViewsId == R.id.view_empty) {
+                viewEmpty = (ViewGroup) view.findViewById(R.id.view_empty);
+                viewEmptyImage = (ImageView) view.findViewById(R.id.imageview_empty_icon);
+                viewEmptyMessage = (TextView) view.findViewById(R.id.textview_empty_message);
+                viewEmptyTryAgainButton = (TextView) view.findViewById(R.id.textview_empty_try_again);
+            } else if (aViewsId == R.id.view_error) {
+                viewError = (ViewGroup) view.findViewById(R.id.view_error);
+                viewErrorImage = (ImageView) view.findViewById(R.id.imageview_error_icon);
+                viewErrorMessage = (TextView) view.findViewById(R.id.textview_error_message);
+                viewErrorTryAgainButton = (TextView) view.findViewById(R.id.button_try_again);
+            } else {
+                viewContainer = view.findViewById(aViewsId);
+                if (viewContainer == null) {
+                    throw new IllegalArgumentException("You should pass the view that will change the visibility with PlaceHolderJ views");
+                }
+            }
+        }
+        if (viewEmpty == null && viewError == null && viewLoading == null) {
+            throw new IllegalArgumentException("You should pass at least one placeholder views to init PlaceHolderJ");
+        }
+        customizeViews();
+    }
+
+    private void customizeViews() {
+        if (placeHolderManager != null && !viewsAreCustomized) {
+            customizeViews.customize(context, viewLoading, viewEmpty, viewEmptyImage, viewEmptyMessage, viewEmptyTryAgainButton, viewError, viewErrorImage, viewErrorMessage, viewErrorTryAgainButton);
+        }
+    }
+
+    private void setViewVisibility(View view, int visibility) {
+        if (view != null) {
+            view.setVisibility(visibility);
+        }
+    }
+
+    /**
+     * Makes the loading view visible if the loaing view is added in your layout.
+     */
+    public void showLoading() {
+        isLoadingViewBeingShown = true;
+        changeViewsVisibility();
+        setViewVisibility(viewLoading, View.VISIBLE);
+    }
+
+    /**
+     * Makes the empty view visible if the empty view is added in your layout.
+     *
+     * @param messageRes      The message that will be show in the empty view.
+     * @param error           The retrofit error get of some of your application requests.
+     * @param onClickListener The action that will be performed by the try again button present in empty view layout.
+     */
+    public void showEmpty(int messageRes, RetrofitError error, View.OnClickListener onClickListener) {
+        viewEmptyMessage.setText(messageRes);
+        showEmpty(error, onClickListener);
+    }
+
+    /**
+     * Makes the empty view visible if the empty view is added in your layout.
+     *
+     * @param error           The retrofit error get of some of your application requests.
+     * @param onClickListener The action that will be performed by the try again button present in empty view layout.
+     */
+    public void showEmpty(RetrofitError error, View.OnClickListener onClickListener) {
+        isEmptyViewBeingShown = true;
+        changeViewsVisibility();
+        if (error == null && viewEmptyTryAgainButton.getVisibility() == View.VISIBLE) {
+            viewEmptyTryAgainButton.setVisibility(View.GONE);
+        } else if (onClickListener != null) {
+            viewEmptyTryAgainButton.setVisibility(View.VISIBLE);
+            viewEmptyTryAgainButton.setOnClickListener(onClickListener);
+        }
+        setViewVisibility(viewEmpty, View.VISIBLE);
+    }
+
+    /**
+     * Makes the error view visible if the error view is added in your layout.
+     *
+     * @param error           The retrofit error get of some of your application requests.
+     * @param onClickListener The action that will be performed by the try again button present in error view layout.
+     */
+    public void showError(RetrofitError error, View.OnClickListener onClickListener) {
+        isErrorViewBeingShown = true;
+        changeViewsVisibility();
+        if (error != null && error.getKind() == RetrofitError.Kind.NETWORK) {
+            viewErrorImage.setImageResource(R.drawable.icon_error_network);
+            viewErrorMessage.setText(R.string.global_network_error);
+        } else {
+            viewErrorImage.setImageResource(R.drawable.icon_error_unknown);
+            viewErrorMessage.setText(R.string.global_unknown_error);
+        }
+        viewErrorTryAgainButton.setOnClickListener(onClickListener);
+        setViewVisibility(viewError, View.VISIBLE);
+    }
+
+    /**
+     * Makes the loading view invisible if the loaing view is added in your layout.
+     */
+    public void hideLoading() {
+        isLoadingViewBeingShown = false;
+        changeViewsVisibility();
+        setViewVisibility(viewLoading, View.GONE);
+    }
+
+    /**
+     * Makes the empty view invisible if the loaing view is added in your layout.
+     */
+    public void hideEmpty() {
+        isEmptyViewBeingShown = false;
+        changeViewsVisibility();
+        setViewVisibility(viewEmpty, View.GONE);
+    }
+
+    /**
+     * Makes the empty view invisible if the loaing view is added in your layout.
+     */
+    public void hideError() {
+        isErrorViewBeingShown = false;
+        changeViewsVisibility();
+        setViewVisibility(viewError, View.GONE);
+    }
+
+    private void changeViewsVisibility() {
+        if (!isLoadingViewBeingShown && viewLoading != null && viewLoading.getVisibility() == View.VISIBLE)
+            setViewVisibility(viewLoading, View.GONE);
+        if (!isEmptyViewBeingShown && viewEmpty != null && viewEmpty.getVisibility() == View.VISIBLE)
+            setViewVisibility(viewEmpty, View.GONE);
+        if (!isEmptyViewBeingShown && viewError != null && viewError.getVisibility() == View.VISIBLE)
+            setViewVisibility(viewError, View.GONE);
+        if (isLoadingViewBeingShown || isEmptyViewBeingShown || isErrorViewBeingShown)
+            setViewVisibility(viewContainer, View.GONE);
+        if (!isLoadingViewBeingShown && !isEmptyViewBeingShown && !isErrorViewBeingShown)
+            setViewVisibility(viewContainer, View.VISIBLE);
+    }
+}
--- placeholderj/src/main/java/com/example/jackmiras/placeholderj/managers/PlaceHolderManager.java
+++ placeholderj/src/main/java/com/example/jackmiras/placeholderj/managers/PlaceHolderManager.java
@@ -1,171 +1,148 @@
 package com.example.jackmiras.placeholderj.managers;
 
-import android.app.Activity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.example.jackmiras.placeholderj.R;
-
-import retrofit.RetrofitError;
-
 /**
- * Created by jackmiras on 03/10/15.
+ * Created by jackson on 12/12/15.
  */
 public class PlaceHolderManager {
 
-    private View viewContainer = null;
-    private View framelayoutViewLoading = null;
-    private ViewGroup linearlayoutViewEmpty = null;
-    private TextView textViewEmptyMessage;
-    private TextView textViewEmptyTryAgain = null;
-    private ViewGroup linearlayoutViewError = null;
-    private ImageView imageViewErrorIcon = null;
-    private TextView textViewErrorMessage = null;
-    private TextView textViewErrorTryAgain = null;
-    private boolean isLoadingViewBeingShown;
-    private boolean isErrorViewBeingShown;
-    private boolean isEmptyViewBeingShown;
+    public int viewErrorBackgroundColor;
+    public int viewErrorBackgroundResource;
+    public int viewErrorText;
+    public int viewErrorTextSize;
+    public int viewErrorTextColor;
+    public int viewErrorTryAgainButtonText;
+    public int viewErrorTryAgainButtonBackgroundResource;
+    public int viewErrorImage;
+    public int viewLoadingBackgroundColor;
+    public int viewLoadingBackgroundResource;
+    public int viewEmptyBackgroundColor;
+    public int viewEmptyBackgroundResource;
+    public int viewEmptyText;
+    public int viewEmptyTextSize;
+    public int viewEmptyTextColor;
+    public int viewEmptyTryAgainButtonText;
+    public int viewEmptyTryAgainButtonBackgroundResource;
+    public int viewEmptyImage;
 
-    /**
-     * Called when a new instance of this class is create. The constructor will get all views passed
-     * in viewsId using the Activity passed together whit viewsId.
-     *
-     * @param activity The activity used to find the views that will be passeds in viewsId.
-     * @param viewsId  A set of view that will interact between himself.
-     */
-    public PlaceHolderManager(Activity activity, int... viewsId) {
-        this(activity.getWindow().getDecorView(), viewsId);
+    public PlaceHolderManager(int viewErrorBackgroundColor, int viewErrorBackgroundResource, int viewErrorText, int viewErrorTextSize, int viewErrorTextColor, int viewErrorTryAgainButtonText, int viewErrorTryAgainButtonBackgroundResource, int viewErrorImage, int viewLoadingBackgroundColor, int viewLoadingBackgroundResource, int viewEmptyBackgroundColor, int viewEmptyBackgroundResource, int viewEmptyText, int viewEmptyTextSize, int viewEmptyTextColor, int viewEmptyTryAgainButtonText, int viewEmptyTryAgainButtonBackgroundResource, int viewEmptyImage) {
+        this.viewErrorBackgroundColor = viewErrorBackgroundColor;
+        this.viewErrorBackgroundResource = viewErrorBackgroundResource;
+        this.viewErrorText = viewErrorText;
+        this.viewErrorTextSize = viewErrorTextSize;
+        this.viewErrorTextColor = viewErrorTextColor;
+        this.viewErrorTryAgainButtonText = viewErrorTryAgainButtonText;
+        this.viewErrorTryAgainButtonBackgroundResource = viewErrorTryAgainButtonBackgroundResource;
+        this.viewErrorImage = viewErrorImage;
+        this.viewLoadingBackgroundColor = viewLoadingBackgroundColor;
+        this.viewLoadingBackgroundResource = viewLoadingBackgroundResource;
+        this.viewEmptyBackgroundColor = viewEmptyBackgroundColor;
+        this.viewEmptyBackgroundResource = viewEmptyBackgroundResource;
+        this.viewEmptyText = viewEmptyText;
+        this.viewEmptyTextSize = viewEmptyTextSize;
+        this.viewEmptyTextColor = viewEmptyTextColor;
+        this.viewEmptyTryAgainButtonText = viewEmptyTryAgainButtonText;
+        this.viewEmptyTryAgainButtonBackgroundResource = viewEmptyTryAgainButtonBackgroundResource;
+        this.viewEmptyImage = viewEmptyImage;
     }
 
-    /**
-     * Called when a new instance of this class is create. The constructor will get all views passed
-     * in viewsId using the Activity passed together whit viewsId.
-     *
-     * @param view The view used to find the views that will be passeds in viewsId.
-     * @param viewsId  A set of view that will interact between himself.
-     */
-    public PlaceHolderManager(View view, int... viewsId) {
-        for (int aViewsId : viewsId) {
-            if (aViewsId == R.id.view_loading) {
-                framelayoutViewLoading = view.findViewById(R.id.view_loading);
-            } else if (aViewsId == R.id.view_empty) {
-                linearlayoutViewEmpty = (ViewGroup) view.findViewById(R.id.view_empty);
-                textViewEmptyMessage = (TextView) view.findViewById(R.id.textview_empty_message);
-                textViewEmptyTryAgain = (TextView) view.findViewById(R.id.textview_empty_try_again);
-            } else if (aViewsId == R.id.view_error) {
-                linearlayoutViewError = (ViewGroup) view.findViewById(R.id.view_error);
-                imageViewErrorIcon = (ImageView) view.findViewById(R.id.imageview_error_icon);
-                textViewErrorMessage = (TextView) view.findViewById(R.id.textview_error_message);
-                textViewErrorTryAgain = (TextView) view.findViewById(R.id.textview_error_try_again);
-            } else {
-                viewContainer = view.findViewById(aViewsId);
-            }
+
+    public static class Configurator {
+        private static int viewErrorBackgroundColor;
+        private static int viewErrorBackgroundResource;
+        private static int viewErrorText;
+        private static int viewErrorTextSize;
+        private static int viewErrorTextColor;
+        private static int viewErrorTryAgainButtonText;
+        private static int viewErrorTryAgainButtonBackgroundResource;
+        private static int viewErrorImage;
+        private static int viewLoadingBackgroundColor;
+        private static int viewLoadingBackgroundResource;
+        private static int viewEmptyBackgroundColor;
+        private static int viewEmptyBackgroundResource;
+        private static int viewEmptyText;
+        private static int viewEmptyTextSize;
+        private static int viewEmptyTextColor;
+        private static int viewEmptyTryAgainButtonText;
+        private static int viewEmptyTryAgainButtonBackgroundResource;
+        private static int viewEmptyImage;
+
+        /**
+         * Error view configurations
+         */
+        public Configurator errorBackgroundColor(int color) {
+            viewErrorBackgroundColor = color;
+            return this;
         }
-    }
 
-    private void setViewVisibility(View view, int visibility) {
-        if (view != null)
-            view.setVisibility(visibility);
-    }
-
-    /**
-     * Makes the loading view visible if the loaing view is added in your layout.
-     */
-    public void showLoading() {
-        isLoadingViewBeingShown = true;
-        changeViewsVisibility();
-        setViewVisibility(framelayoutViewLoading, View.VISIBLE);
-    }
-
-    /**
-     * Makes the empty view visible if the empty view is added in your layout.
-     *
-     * @param messageRes      The message that will be show in the empty view.
-     * @param error           The retrofit error get of some of your application requests.
-     * @param onClickListener The action that will be performed by the try again button present in empty view layout.
-     */
-    public void showEmpty(int messageRes, RetrofitError error, View.OnClickListener onClickListener) {
-        textViewEmptyMessage.setText(messageRes);
-        showEmpty(error, onClickListener);
-    }
-
-    /**
-     * Makes the empty view visible if the empty view is added in your layout.
-     *
-     * @param error           The retrofit error get of some of your application requests.
-     * @param onClickListener The action that will be performed by the try again button present in empty view layout.
-     */
-    public void showEmpty(RetrofitError error, View.OnClickListener onClickListener) {
-        isEmptyViewBeingShown = true;
-        changeViewsVisibility();
-        if (error == null && textViewEmptyTryAgain.getVisibility() == View.VISIBLE) {
-            textViewEmptyTryAgain.setVisibility(View.GONE);
-        } else if (onClickListener != null) {
-            textViewEmptyTryAgain.setVisibility(View.VISIBLE);
-            textViewEmptyTryAgain.setOnClickListener(onClickListener);
+        public Configurator errorBackgroundResource(int backgroundRes) {
+            viewErrorBackgroundResource = backgroundRes;
+            return this;
         }
-        setViewVisibility(linearlayoutViewEmpty, View.VISIBLE);
-    }
 
-    /**
-     * Makes the error view visible if the error view is added in your layout.
-     *
-     * @param error           The retrofit error get of some of your application requests.
-     * @param onClickListener The action that will be performed by the try again button present in error view layout.
-     */
-    public void showError(RetrofitError error, View.OnClickListener onClickListener) {
-        isErrorViewBeingShown = true;
-        changeViewsVisibility();
-        if (error != null && error.getKind() == RetrofitError.Kind.NETWORK) {
-            imageViewErrorIcon.setImageResource(R.drawable.icon_error_network);
-            textViewErrorMessage.setText(R.string.global_network_error);
-        } else {
-            imageViewErrorIcon.setImageResource(R.drawable.icon_error_unknown);
-            textViewErrorMessage.setText(R.string.global_unknown_error);
+        public Configurator errorText(int text, int textSize, int textColor) {
+            viewErrorText = text;
+            viewErrorTextSize = textSize;
+            viewErrorTextColor = textColor;
+            return this;
         }
-        textViewErrorTryAgain.setOnClickListener(onClickListener);
-        setViewVisibility(linearlayoutViewError, View.VISIBLE);
-    }
 
-    /**
-     * Makes the loading view invisible if the loaing view is added in your layout.
-     */
-    public void hideLoading() {
-        isLoadingViewBeingShown = false;
-        changeViewsVisibility();
-        setViewVisibility(framelayoutViewLoading, View.GONE);
-    }
+        public Configurator errorButton(int text, int resource) {
+            viewErrorTryAgainButtonText = text;
+            viewErrorTryAgainButtonBackgroundResource = resource;
+            return this;
+        }
 
-    /**
-     * Makes the empty view invisible if the loaing view is added in your layout.
-     */
-    public void hideEmpty() {
-        isEmptyViewBeingShown = false;
-        changeViewsVisibility();
-        setViewVisibility(linearlayoutViewEmpty, View.GONE);
-    }
+        public Configurator errorImage(int imageRes) {
+            viewErrorImage = imageRes;
+            return this;
+        }
 
-    /**
-     * Makes the empty view invisible if the loaing view is added in your layout.
-     */
-    public void hideError() {
-        isErrorViewBeingShown = false;
-        changeViewsVisibility();
-        setViewVisibility(linearlayoutViewError, View.GONE);
-    }
+        /**
+         * Loading view configurations
+         */
+        public Configurator loadingBackground(int color) {
+            viewLoadingBackgroundColor = color;
+            return this;
+        }
 
-    private void changeViewsVisibility() {
-        if (!isLoadingViewBeingShown && framelayoutViewLoading != null && framelayoutViewLoading.getVisibility() == View.VISIBLE)
-            setViewVisibility(framelayoutViewLoading, View.GONE);
-        if (!isEmptyViewBeingShown && linearlayoutViewEmpty != null && linearlayoutViewEmpty.getVisibility() == View.VISIBLE)
-            setViewVisibility(linearlayoutViewEmpty, View.GONE);
-        if (!isEmptyViewBeingShown && linearlayoutViewError != null && linearlayoutViewError.getVisibility() == View.VISIBLE)
-            setViewVisibility(linearlayoutViewError, View.GONE);
-        if (isLoadingViewBeingShown || isEmptyViewBeingShown || isErrorViewBeingShown)
-            setViewVisibility(viewContainer, View.GONE);
-        if (!isLoadingViewBeingShown && !isEmptyViewBeingShown && !isErrorViewBeingShown)
-            setViewVisibility(viewContainer, View.VISIBLE);
+        public Configurator loadingBackgroundResource(int backgroundRes) {
+            viewLoadingBackgroundResource = backgroundRes;
+            return this;
+        }
+
+        /**
+         * Empty view configurations
+         */
+        public Configurator emptyBackground(int color) {
+            viewEmptyBackgroundColor = color;
+            return this;
+        }
+
+        public Configurator emptyBackgroundResource(int backgroundResource) {
+            viewEmptyBackgroundResource = backgroundResource;
+            return this;
+        }
+
+        public Configurator emptyText(int text, int textSize, int textColor) {
+            viewEmptyText = text;
+            viewEmptyTextSize = textSize;
+            viewEmptyTextColor = textColor;
+            return this;
+        }
+
+        public Configurator emptyButton(int text, int resource) {
+            viewEmptyTryAgainButtonText = text;
+            viewEmptyTryAgainButtonBackgroundResource = resource;
+            return this;
+        }
+
+        public Configurator emptyImage(int imageRes) {
+            viewEmptyImage = imageRes;
+            return this;
+        }
+
+        public PlaceHolderManager config() {
+            return new PlaceHolderManager(viewErrorBackgroundColor, viewErrorBackgroundResource, viewErrorText, viewErrorTextSize, viewErrorTextColor, viewErrorTryAgainButtonText, viewErrorTryAgainButtonBackgroundResource, viewErrorImage, viewLoadingBackgroundColor, viewLoadingBackgroundResource, viewEmptyBackgroundColor, viewEmptyBackgroundResource, viewEmptyText, viewEmptyTextSize, viewEmptyTextColor, viewEmptyTryAgainButtonText, viewEmptyTryAgainButtonBackgroundResource, viewEmptyImage);
+        }
     }
 }
